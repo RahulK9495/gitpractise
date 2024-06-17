f783205cbd23bebabca1412a80f4c76ee2c93d3d
RahulK9495 <rahulkhot5554@gmail.com>
2024-06-17 15:34:12 +0530

    stashed change

--- src/main/java/com/prowing/practise/hello.java
+++ src/main/java/com/prowing/practise/hello.java
@@ -5,6 +5,7 @@ public class hello {
 	public void hello() {
 		System.out.println("hello..........");
 		System.out.println("substaction developer change...");
+		System.out.println("new change");
 	}
 
 	
